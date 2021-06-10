5e4966daa808c4114635ba82db5a1ddc86869619
TatjanaTomic <tatjanatomic997@yahoo.com>
2021-06-10 15:01:46 +0200

    Ispravkica GUI-ja

--- EncryptedFileSystem/Forms/MyFileForm.Designer.cs
+++ EncryptedFileSystem/Forms/MyFileForm.Designer.cs
@@ -135,7 +135,6 @@ namespace EncryptedFileSystem.Forms
             // 
             // MyFileForm
             // 
-            this.AcceptButton = this.btnSave;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
